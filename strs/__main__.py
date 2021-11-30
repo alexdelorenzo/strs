52d20a312a5e68ccbcb79820fbca0589cea70f1f
Alex DeLorenzo <alex@alexdelorenzo.dev>
2021-11-29 22:15:59 -0500

    Rely on module-level exports to filter commands from other objects

--- strs/__main__.py
+++ strs/__main__.py
@@ -1,27 +1,7 @@
-import types
-
-# from .commands import capitalize, casefold, length, count, \
-#  index, join, split, upper, lower, strip, title, swapcase, \
-#  lstrip, rstrip, find, isascii
 from .commands import *
 
 
-def leave_only_cmd_funcs():
-  for name, val in globals().copy().items():
-    match val:
-      case types.FunctionType() | types.MethodType():
-        pass
-
-      case mod if mod is types:
-        pass
-
-      case _ if not name.startswith('__'):
-        del globals()[name]
-
-
 def _main():
-  leave_only_cmd_funcs()
-
   from fire import Fire as _Fire
 
   len = length
