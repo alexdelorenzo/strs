ede1a8f98918893ff81221228a430765c362ce56
Alex DeLorenzo <alex@alexdelorenzo.dev>
2021-12-07 18:45:06 -0500

    Remove test docstrings

--- strs/__main__.py
+++ strs/__main__.py
@@ -5,12 +5,18 @@ from .plugins import *
 del analyze, default, more, validate, string, transform
 
 
+class T:
+  def print(self):
+    print('test')
+
+
 def _main():
   from fire import Fire as _Fire
-
+  ___doc___ = 'test'
   len = length
 
   _Fire()
+  # _Fire(T)
 
 
 if __name__ == "__main__":
