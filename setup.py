df5326c4b09e7860ee0b09481c7f9ba415c6e4e8
Alex DeLorenzo <alex@alexdelorenzo.dev>
2021-12-08 00:07:42 -0500

    Increment version with minor bump for bugfix

--- setup.py
+++ setup.py
@@ -17,13 +17,12 @@ pkgs: list[str] = find_packages(
 
 setup(
   name="strs",
-  version="0.2.0",
+  version="0.2.1",
   description="🧵 Easy string tools for the shell",
   url="https://github.com/alexdelorenzo/strs",
   author="Alex DeLorenzo",
   license="AGPL 3.0",
   packages=pkgs,
-  zip_safe=True,
   install_requires=requirements,
   python_requires='>=3.10',
   entry_points={
