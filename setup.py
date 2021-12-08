b25a1d51c8f65554c0ea931abbc4948ce869e3bf
Alex DeLorenzo <alex@alexdelorenzo.dev>
2021-12-08 18:51:37 -0500

    Increment version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ pkgs: list[str] = find_packages(
 
 setup(
   name="strs",
-  version="0.2.1",
+  version="0.2.3",
   description="🧵 Easy string tools for the shell",
   url="https://github.com/alexdelorenzo/strs",
   author="Alex DeLorenzo",
