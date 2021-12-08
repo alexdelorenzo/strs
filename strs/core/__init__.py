659f89e8b967fe262ee93c0d19b77dffa83b60f6
Alex DeLorenzo <alex@alexdelorenzo.dev>
2021-12-07 21:06:05 -0500

    Set default name for CLI

--- strs/core/__init__.py
+++ strs/core/__init__.py
@@ -1,6 +1,12 @@
+from __future__ import annotations
+from typing import Final
+
 from .base import *
 from .constants import *
 from .decorators import *
 from .input import *
 from .process import *
 from .types import *
+
+
+NAME: Final[str] = 'strs'
