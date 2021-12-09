d0bcea7ad68172b7414a672f99a3a9fefebb7f66
Alex DeLorenzo <alex@alexdelorenzo.dev>
2021-12-08 19:41:34 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ pkgs: list[str] = find_packages(
 
 setup(
   name="strs",
-  version="0.2.3",
+  version="0.2.4",
   description="🧵 Easy string tools for the shell",
   url="https://github.com/alexdelorenzo/strs",
   author="Alex DeLorenzo",
