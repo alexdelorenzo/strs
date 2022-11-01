f4278a23c10f891c0658e6b25029605075f35947
Alex DeLorenzo <alex@alexdelorenzo.dev>
2022-11-01 08:03:40 -0400

    Increment version, bump Python to 3.11

--- setup.py
+++ setup.py
@@ -17,14 +17,14 @@ pkgs: list[str] = find_packages(
 
 setup(
   name="strs",
-  version="0.2.9",
+  version="0.3.0",
   description="🧵 Easy string tools for the shell",
   url="https://github.com/alexdelorenzo/strs",
   author="Alex DeLorenzo",
   license="AGPL 3.0",
   packages=pkgs,
   install_requires=requirements,
-  python_requires='>=3.10',
+  python_requires='>=3.11',
   entry_points={
     "console_scripts": [
       "strs = strs.__main__:_main",
